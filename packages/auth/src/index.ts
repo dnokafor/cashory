fa5cab1e35cd01ced5ffe0e9a465615e6eadbc6d
shakibdshy <shakibdshy@gmail.com>
2026-03-20 07:23:25 +0600

    initial commit

---
+++ packages/auth/src/index.ts
@@ -0,0 +1,34 @@
+import { expo } from "@better-auth/expo";
+import { db } from "@cashory-demo/db";
+import * as schema from "@cashory-demo/db/schema/auth";
+import { env } from "@cashory-demo/env/server";
+import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+
+export const auth = betterAuth({
+  database: drizzleAdapter(db, {
+    provider: "pg",
+
+    schema: schema,
+  }),
+  trustedOrigins: [
+    env.CORS_ORIGIN,
+    "cashory-demo://",
+    ...(env.NODE_ENV === "development"
+      ? ["exp://", "exp://**", "exp://192.168.*.*:*/**", "http://localhost:8081"]
+      : []),
+  ],
+  emailAndPassword: {
+    enabled: true,
+  },
+  secret: env.BETTER_AUTH_SECRET,
+  baseURL: env.BETTER_AUTH_URL,
+  advanced: {
+    defaultCookieAttributes: {
+      sameSite: "none",
+      secure: true,
+      httpOnly: true,
+    },
+  },
+  plugins: [expo()],
+});
